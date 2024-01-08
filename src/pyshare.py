2df2c1fb4150c5cabc1ddb7b1352ad86bc79a3e0
Alper Akkın <alperakkin@gmail.com>
2024-01-08 16:30:28 +0300

    queue mechanism added

--- src/pyshare.py
+++ src/pyshare.py
@@ -1,7 +1,7 @@
 import socket
 import pickle
 from threading import Thread
-
+from queue import Queue
 from libs.utils import load_config, get_local_ip
 
 
@@ -14,13 +14,14 @@ class PyShare:
         self.receiver_socket = self.create_socket()
 
         self.receiver = get_local_ip()
-        self.receiver_port = config['receiver']['port']
+        self.receiver_port = config['receive']['port']
 
-        self.sender = config['sender']['address']
-        self.sender_port = config['sender']['port']
+        self.sender = config['to_send']['address']
+        self.sender_port = config['to_send']['port']
 
         self.listen_incoming = Thread(target=self.listen, daemon=True)
         self.listen_incoming.start()
+        self.queue = Queue()
 
     def listen(self) -> None:
         self.receiver_socket.bind((self.receiver, self.receiver_port))
@@ -33,7 +34,7 @@ class PyShare:
                 if not data:
                     continue
                 conn.send(b'Data is received!')
-                self.data = pickle.loads(data)
+                self.queue.put(pickle.loads(data))
 
     def create_socket(self) -> socket.socket:
         return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,8 +42,11 @@ class PyShare:
     def connect(self) -> None:
         self.sender_socket.connect((self.sender, self.sender_port))
 
-    def send_obj(self, obj: object) -> bool:
+    def send_obj(self, obj: object) -> str:
         self.connect()
         serialized = pickle.dumps(obj)
         self.sender_socket.send(serialized)
         return self.sender_socket.recv(self.BANDWITH)
+
+    def receive_obj(self) -> object:
+        return self.queue.get()
