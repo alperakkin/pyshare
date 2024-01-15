36d14dda80b34536a9285dee5bcaef30ce8dcad3
Alper Akkın <alperakkin@gmail.com>
2024-01-15 14:58:34 +0300

    working example

--- src/pyshare.py
+++ src/pyshare.py
@@ -1,7 +1,7 @@
 import socket
 import pickle
 from threading import Thread
-from queue import Queue
+
 from libs.utils import load_config, get_local_ip
 
 
@@ -22,7 +22,10 @@ class PyShare:
 
         self.listen_incoming = Thread(target=self.listen, daemon=True)
         self.listen_incoming.start()
-        self.queue = Queue()
+        self.scope = {}
+
+    def attach(self, name: str, obj: object) -> None:
+        self.scope[name] = obj
 
     def listen(self) -> None:
         self.receiver_socket.bind((self.receiver, self.receiver_port))
@@ -34,14 +37,11 @@ class PyShare:
                 data = conn.recv(self.BANDWITH)
                 if not data:
                     continue
+
                 if data.startswith(b'<request>: '):
-                    obj = eval(data.decode().lstrip('<request>: '))
-                    res = self.send_obj(obj, (self.receiver,
-                                              self.receiver_port))
-                    print('PyObject is sent: %s ' % res)
-                else:
-                    conn.send(b'Data is received!')
-                    self.queue.put(pickle.loads(data))
+                    obj_name = data.decode().lstrip('<request>: ')
+                    obj = self.scope.get(obj_name)
+                    self.send_obj(conn, obj)
 
     def create_socket(self) -> socket.socket:
         return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -49,20 +49,17 @@ class PyShare:
     def connect(self, connection) -> None:
         self.sender_socket.connect(connection)
 
-    def send_obj(self, obj: object, connection: tuple) -> str:
-        if not self.connected:
-            self.connect(connection)
-            self.connected = True
+    def send_obj(self, conn: object, obj: object) -> str:
         serialized = pickle.dumps(obj)
-        self.sender_socket.send(serialized)
-        return self.sender_socket.recv(self.BANDWITH)
-
-    def receive_obj(self) -> object:
-        return self.queue.get()
+        conn.send(serialized)
+        return conn.recv(self.BANDWITH)
 
     def import_from(self, object_name: str, connection: tuple) -> object:
         if not self.connected:
             self.connect(connection)
             self.connected = True
-        self.sender_socket.send(b'<request>: %s' % object_name)
-        return self.sender_socket.recv(self.BANDWITH)
+        request_string = '<request>: %s' % object_name
+        self.sender_socket.send(request_string.encode())
+        obj = self.sender_socket.recv(self.BANDWITH)
+
+        return pickle.loads(obj)
